c9f495dff59f8184504aeeedebf60ef2e76b4ea2
kristinaz <k.ziedavainyte@gmail.com>
2013-09-16 09:24:43 +0300

    papildymas 4 isvestine

--- TestoUzdavinys2.m
+++ TestoUzdavinys2.m
@@ -11,6 +11,6 @@ function [ f,f_isv,f_isv2, f_isv3, f_isv4 ] = TestoUzdavinys2( x )
     f_isv = cos(x) - sin(x);
     f_isv2 = - sin(x) - cos(x);
     f_isv3 = - cos(x) + sin(x);
-    f_isv4 = sin(x) - cos(x);
+    f_isv4 = sin(x) + cos(x);
 end
 
