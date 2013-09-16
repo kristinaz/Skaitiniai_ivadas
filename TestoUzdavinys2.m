d8eed237aa9b56efc59002247cd0f9588fdb97ae
kristinaz <k.ziedavainyte@gmail.com>
2013-09-16 08:40:32 +0300

    Papildziau vienamciu uzdaviniu

---
+++ TestoUzdavinys2.m
@@ -0,0 +1,14 @@
+function [ f,f_isv,f_isv2 ] = TestoUzdavinys2( x )
+%Antrojo testo uzdavinio aprasas.
+%INPUT(ivesties parametrai): x -realus vienamtis vektorius (skaliaras)
+%OUTPUT(isvesties parametrai): 
+% f - funkcijos reiksme taske x,
+% f'(x) - pirmosios isvestines reiksme taske x, f'(x)
+% f"(x) - antrosios isvestines reiksme taske x, f"(x)
+%pvz.: [f,f_isv,f_isv2] = TestoUzdavinys1 ([1,1])
+% arba x=[1,1]; [f,f_isv,f_isv2] = TestoUzdavinys1 (x)
+    f= sin(x) + cox(x);
+    f_isv = cos(x) - sin(x);
+    f_isv2 = - sin(x) - cos(x);
+end
+
